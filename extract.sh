4724315c8e7afa1e261ce9c7fe0e1f8a81d1c309
Yizhang Cao <tigercao1@gmail.com>
2019-10-15 15:40:44 -0400

    use generic path

--- extract.sh
+++ extract.sh
@@ -1,9 +1,8 @@
-for path in /Users/yizhangcao/Desktop/TA/2401/"${1}"/*; do
+for path in "${1}"/*; do
     [ -d "${path}" ] || continue # if not a directory, skip
     dirname="$(basename "${path}")"
     echo "****************************************************"
     echo "${dirname}"
     echo "****************************************************"
     tar -C "${path}" -xvf "${path}"/*.tar
-    echo $?    
 done
